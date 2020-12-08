2acc2b1c871dca204ff3f6965104d9aa35c6a17c
motokovil <65578314+motokovil@users.noreply.github.com>
2020-12-08 02:24:16 -0500

    ok

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Publicacion(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField(max_length=340)
-
+    
     def __str__(self):
         return self.titulo
